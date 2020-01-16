6ef60e3301adde418e906d4c7aa1f3b4e263cef3
Michał Knapik <mknapik@users.noreply.github.com>
2020-01-16 19:11:11 +0100

    Improve keycounter by saving key symbols.

--- src/keycounter.cpp
+++ src/keycounter.cpp
@@ -11,34 +11,40 @@
 #include "event.h"
 #include "keyboard-event-mapping.hpp"
 
+const auto log_path = "/var/log/interception/keycounter.log";
+const auto separator = ',';
+
 int main(int argc, char* argv[]) {
   int is_mapped_key_down = 0, compose_give_up = 0;
   struct input_event input;
   const struct input_event *mapped_down, *mapped_repeat, *mapped_up;
 
-  auto counter = std::map<std::string, unsigned long>();
+  std::map<const std::string, unsigned long> counter;
 
   setbuf(stdin, NULL), setbuf(stdout, NULL);
 
-  std::ifstream prev("/var/log/interception-keycoutner.log");
-  if (prev.is_open()) {
-    for (std::string line; std::getline(prev, line);) {
-      int pos = line.find_first_of(':');
-      std::string count = line.substr(pos + 1), symbol = line.substr(0, pos);
-      counter[symbol] = stoi(count);
+  {
+    std::ifstream prev(log_path);
+    if (prev.is_open()) {
+      for (std::string line; std::getline(prev, line);) {
+        const int pos = line.find_first_of(separator);
+        std::string count = line.substr(pos + 1), symbol = line.substr(0, pos);
+        counter[symbol] = stoi(count);
+      }
     }
+    prev.close();
   }
 
   short c = 0;
   while (read_event(&input)) {
     if (input.type == EV_KEY && input.value == 1) {
+      const auto code = keyboard_event_mapping[input.code];
+      counter[code] = counter[code] + 1;
 
-      counter[input.code] = counter[input.code] + 1;
-
-      if (c++ % 10 == 0) {
-        std::ofstream myfile("/var/log/interception-keycoutner.log");
+      if (c++ % 100 == 0) {
+        std::ofstream myfile(log_path);
         for (auto it = counter.begin(); it != counter.end(); it++) {
-          myfile << it->first << ":" << it->second << "\n";
+          myfile << it->first << separator << it->second << "\n";
         }
         myfile.flush();
         myfile.close();
